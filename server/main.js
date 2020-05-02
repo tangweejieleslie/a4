0a48f540f505d00ba30d7eba21abc7b0e0f4c333
Leslie Tang <tangweejieleslie+github@gmail.com>
2020-05-02 23:41:41 +0800

    data now added into mongoDB collections

--- server/main.js
+++ server/main.js
@@ -1,13 +1,12 @@
 import { Meteor } from "meteor/meteor";
-import { LinksCollection } from "/imports/api/links";
+import { TemperatureDataCollections } from "/imports/api/temperatureData";
 import * as Papa from "papaparse";
 
 function insertLink({ title, url }) {
   LinksCollection.insert({ title, url, createdAt: new Date() });
 }
 
-function csvToJson(csvData) {
-
+function csvToArray(csvData) {
   // Note: index
   // 0: roomNumber
   // 1: date
@@ -34,7 +33,7 @@ function csvToJson(csvData) {
   // Iterate through all CSV data, and match data by date TODO: could be optimized if there's time
   for (let i = 0; i < distinctDates.length; i++) {
     for (let j = 1; j < csvData.length; j++) {
-      if(distinctDates[i] == csvData[j][1]){
+      if (distinctDates[i] == csvData[j][1]) {
         let roomNum = parseInt(csvData[j][0]);
         let temp = parseFloat(csvData[j][2]);
         finalData[i][roomNum] = temp;
@@ -45,11 +44,32 @@ function csvToJson(csvData) {
 }
 
 Meteor.startup(() => {
+  // Init with empty database (to prevent duplicate data entry)
+  console.log("Removing data from MongoDB...");
+  TemperatureDataCollections.remove({});
+
+  // Parsing data
+  console.log("Parsing data from 'room-temperature.csv'...");
   const csv = Assets.getText("room-temperatures.csv");
   const data = Papa.parse(csv).data;
-  // console.log(data[0]);
-  // console.log(data[1]);
-  // console.log(data[3][0]);
-  const endData = csvToJson(data);
-  console.log(endData[0]);
+  const csvArray = csvToArray(data); // 2d array index: 0-6 represents room temperature, 7 represents temperature
+  // console.log("DATA: " + csvArray[0][0]);
+
+  // Init with newly parsed csv data
+  console.log("Adding data to MongoDB...");
+  for (let i = 0; i < csvArray.length; i++) {
+    let temp = {
+      date: csvArray[i][7],
+      roomTemperature: {
+        room0: csvArray[i][0],
+        room1: csvArray[i][1],
+        room2: csvArray[i][2],
+        room3: csvArray[i][3],
+        room4: csvArray[i][4],
+        room5: csvArray[i][5],
+        room6: csvArray[i][6],
+      },
+    };
+    TemperatureDataCollections.insert(temp);
+  }
 });
