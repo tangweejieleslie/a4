a005f1a3fdb5257d5e4e6e8731a513d9947b185a
Leslie Tang <tangweejieleslie+github@gmail.com>
2020-05-03 20:10:02 +0800

    added date and time handler

--- imports/ui/components/GraphControls.jsx
+++ imports/ui/components/GraphControls.jsx
@@ -2,37 +2,74 @@ import React, { Component } from "react";
 import Graph from "./Graph";
 
 class Template extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-          startDate: "2013-10-03",
-          startTime: "15:00:00",
-          endDate: "2013-10-03",
-          endTime: "15:15:00",
-          sampleSize: 0
-      };
-    }
-  
-
-    handleChange(id){
-        console.log("Something changed - " + id)
-    }
-
-    render() {
-      return (
-        <div className="Template">
-            Start Date <input type="date" defaultValue={this.state.startDate} onChange={this.handleChange(1)}></input>
-            End Date <input type="date" defaultValue={this.state.endDate} onChange={this.handleChange(2)}></input>
-            <br></br>
-            Start Time <input type="time" defaultValue={this.state.startTime} onChange={this.handleChange(3)}></input>
-            End Time <input type="time" defaultValue={this.state.endTime} onChange={this.handleChange(4)}></input>
-            <br></br>
-            <br></br>
-            <Graph />
-        </div>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: "2013-10-03",
+      startTime: "15:00:00",
+      endDate: "2013-10-03",
+      endTime: "15:15:00",
+      sampleSize: 0,
+    };
   }
-  
-  export default Template;
-  
+
+  handleStartDate(event) {
+    this.setState({
+      startDate: event.target.value,
+    });
+  }
+
+  handleEndDate(event) {
+    this.setState({
+      endDate: event.target.value,
+    });
+  }
+
+  handleStartTime(event) {
+    this.setState({
+      startTime: event.target.value,
+    });
+  }
+
+  handleEndTime(event) {
+    this.setState({
+      endTime: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div className="Template">
+        Start Date
+        <input
+          type="date"
+          value={this.state.startDate}
+          onChange={() => this.handleStartDate(event)}
+        />
+        End Date
+        <input
+          type="date"
+          value={this.state.endDate}
+          onChange={() => this.handleEndDate(event)}
+        />
+        <br></br>
+        Start Time
+        <input
+          type="time"
+          value={this.state.startTime}
+          onChange={() => this.handleStartTime(event)}
+        />
+        End Time
+        <input
+          type="time"
+          value={this.state.endTime}
+          onChange={() => this.handleEndTime(event)}
+        />
+        <br></br>
+        <Graph />
+      </div>
+    );
+  }
+}
+
+export default Template;
